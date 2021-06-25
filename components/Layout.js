a0a7aa11f78d188ad7c7cbd47ad4a89851c69497
Huda Brilian <hudabrilian@gmail.com>
2021-06-26 00:45:31 +0700

    add heading

--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,17 @@ import Footer from "./Footer"
 
 function Layout({ children }) {
     return (
-        <div className="min-h-screen h-screen">
-            <Navbar />
-            { children }
-            <Footer />
-        </div>
+        <>
+            <Head>
+                <title>Brilian.</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <div className="min-h-screen h-screen">
+                <Navbar />
+                { children }
+                <Footer />
+            </div>
+        </>
     )
 }
 
